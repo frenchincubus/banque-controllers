51f51837a2cf2a045b02fcdb720625d89b0518d7
frenchincubus <lionelrenier82@gmail.com>
2021-01-22 15:33:49 +0100

    #1 - init project

---
+++ src/main/java/fr/banque/BanqueControllersApplication.java
@@ -0,0 +1,13 @@
+package fr.banque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueControllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueControllersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/banque/BanqueControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.banque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueControllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
